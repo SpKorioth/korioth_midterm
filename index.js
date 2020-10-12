0300728fd1d4636da6b98633d31cd2b9836f89dc
ks-csu <korioth_spencer@columbusstate.edu>
2020-10-12 19:58:18 +0000

    initial empty page

---
+++ index.js
@@ -0,0 +1,30 @@
+//require express
+var express =require('express');
+//require node-fetch
+var fetch = require('node-fetch');
+
+//create express object, call express
+var app = express();
+
+//get port information
+const port = process.env.PORT || 3000;
+
+//tell application to use EJS for templates
+app.set('view engine', 'ejs');
+
+//make styles public
+app.use(express.static("public"));
+
+//get home page /
+app.get('/',function(req, res){
+    //return something to home page
+    res.render('index');
+})
+
+//get/post here
+
+
+//server setup
+app.listen(port, function(){
+    console.log('Listening on ' + port)
+});
